f019881013c87637bb6d8b56ad82503a46263a60
Muhammad Abdurraheem <1501918@sit.singaporetech.edu.sg>
2023-02-26 12:56:51 +0800

    ServiceCentreModel and RMMIS DB setup

---
+++ RMMIS/Migrations/20230226044049_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMMIS.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ServiceCentre",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    EstablishedDate = table.Column<string>(type: "TEXT", nullable: false),
+                    Manager = table.Column<string>(type: "TEXT", nullable: false),
+                    NoOfProjects = table.Column<int>(type: "INTEGER", nullable: true),
+                    NoOfEmployees = table.Column<int>(type: "INTEGER", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceCentre", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServiceCentre");
+        }
+    }
+}
---
+++ RMMIS/Models/ServiceCentreModel.cs
@@ -0,0 +1,15 @@
+﻿namespace RMMIS.Models
+	{
+	public class ServiceCentreModel
+		{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string EstablishedDate { get; set; }
+		public string Manager { get; set; }
+		public int NoOfProjects { get; set; }
+		public int NoOfEmployees { get; set; }
+
+
+		}
+	}
